7267324673b211dca5cc174435c91d91592621f3
Benjamin Knopp <benjaminknoppster@gmail.com>
2021-03-06 19:39:56 +0100

    Add files to generate beeps.

---
+++ sounds/generate_audio.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from subprocess import run
+
+D = pd.read_csv('data/data_x.txt', sep='\t', header=None)
+print(D)
+
+d = D.values.flatten()
+d = d.astype(int)
+d = set(d[~np.isnan(d)])
+d = sorted(d)
+print(d)
+
+for ms in d:
+    cmd = f"ffmpeg -i sounds/Sine_wave_440.ogg -ss 00:00:00.000 -t 00:00:00.{ms} -c copy sounds/{ms}_ms.ogg"
+    print(cmd)
+    out = run(cmd.split())
+    print(out.returncode)
