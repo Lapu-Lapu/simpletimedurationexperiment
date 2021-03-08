fdbac3cbd5d1dd04962f1737cc1713fb43165da9
Benjamin Knopp <knoppbe@staff.uni-marburg.de>
2021-03-08 21:13:15 +0100

    Include more data.

--- analysis/read_data.py
+++ analysis/read_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
 from itertools import zip_longest
+from collections import defaultdict
 
 
 def get_duration(s):
@@ -30,20 +31,30 @@ def get_new_line(lst):
 
 if __name__ == '__main__':
     files = glob("data/experiment_data_*_*.csv")
+    name_dict = defaultdict(list)
     with open('analysis/files.txt') as fh:
-        files = [f'experiment/data/{f[:-1]}' for f in fh]
-    print(files)
-    data = []
-    for fn in files:
-        name, identifier = fn.split('.csv')[0].split('_')[-2:]
-        # if not 'lrjs' in identifier:
-        #     continue  # this is tabea
-        d = pd.read_csv(fn, sep=',')
-        d['name'] = name
-        d['id'] = identifier
-        print(name, identifier, d.shape)
-        data.append(d)
-    data = pd.concat(data)
+        for line in fh:
+            name, fn = line.rstrip().split(',')
+            print(fn)
+            name_dict[name].append(fn)
+    files = [f'experiment/data/{f}' for f in name_dict.values()]
+    print(name_dict)
+
+    datalst = []
+    for name, fns in name_dict.items():
+        for fn in fns:
+            print(fn)
+            name2, identifier = fn.split('.csv')[0].split('_')[-2:]
+            if name != name2:
+                print(name2, "is now called", name)
+            # if not 'lrjs' in identifier:
+            #     continue  # this is tabea
+            d = pd.read_csv(f'experiment/data/{fn}', sep=',')
+            d['name'] = name
+            d['id'] = identifier
+            print(name, identifier, d.shape)
+            datalst.append(d)
+    data = pd.concat(datalst, ignore_index=True)
     idxs = ~np.isnan(data.response)
     data = data[idxs]
     # print(data)
@@ -56,9 +67,11 @@ if __name__ == '__main__':
     for name, d in gb:
         # d['duration'] = d['stimulus'].apply(get_duration)
         data_rprop[name] = d.groupby('duration').response.mean()
+        # print(data_rprop[name])
         data_x[name] = data_rprop[name].index
         # print(rprop)
         data_n[name] = d.groupby('duration').response.agg(len)
+        # print(data_n[name])
         data_r[name] = d.groupby('duration').response.sum()
     # rprop.plot(style='.')
     # plt.show()
